b44e76c505e0220a4b6836ea325a8c86454e6853
Muhammad Owais Ansari <owais.ansaricr7@gmail.com>
2023-09-01 04:30:49 +0500

    Add project files.

---
+++ CRUDApi/Data/FullStackDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CRUDApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDApi.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
